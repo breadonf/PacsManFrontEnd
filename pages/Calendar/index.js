327bf4f6923375da2e660b573b7cdfdb4285a2f3
Will Lee <you@example.com>
2021-12-12 23:13:33 +0800

    fix calendar and beautify api

--- pages/Calendar/index.js
+++ pages/Calendar/index.js
@@ -1,29 +1,28 @@
 import React from "react";
 import TaskCalendar from "../../components/tasks/TaskCalendar";
 import axios from 'axios';
-import { useState } from "react";
+import {
+  useState
+} from "react";
+import useSWR from 'swr'
+
 
 const actToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiZW1haWwiOiJoa2NoQGdtYWlsLmNvbSIsIm5hbWUiOiJoa2NoYWRtaW4iLCJpZCI6IjYxOTQ1ZjU0NjdmZDYwNmJlYzFlZDBmNSIsInJvbGUiOiJhZG1pbiIsImlhdCI6MTYzOTI5NTg4OSwiZXhwIjoxNjM5MzgyMjg5fQ.rA242juUiv7_E0HAX_NOJFky_ATqDA8DDAZpgFSjOzc"
 const apiUrl = "https://backend-productivity.herokuapp.com/tasks/api/get-task/"
 
-async function res () {
-  const res = await axios({
-    method: 'get',
-    url: apiUrl,
-    headers: {'Authorization': `Bearer ${actToken}`}
-  })
-  const data = res.data.message
-  console.log(data)
-}
-res()
+const fetcher = url => axios({method: "get", url: url, headers: {'Authorization': `Bearer ${actToken}`}}).then(res => res.data.message)
 
 
 
-function Calendar({data}) {
-  return (
-    <TaskCalendar events={data} />
+
+
+function Calendar() {
+  const {data, error} =useSWR(apiUrl, fetcher)
+  if (error) return <div>Failed to load</div>
+  if(!data) return <div>Loading</div>
+  return ( 
+    <TaskCalendar events = {data}/>
   )
 }
-
 
 export default Calendar;
