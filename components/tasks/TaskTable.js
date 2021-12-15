199307ee08055ae7029f6baec65d2ad2fbacbc38
Will Lee <willlee19951009@gmail.com>
2021-12-15 18:47:37 +0800

    deploy frontend

--- components/tasks/TaskTable.js
+++ components/tasks/TaskTable.js
@@ -35,11 +35,11 @@ const columns = [
       
         const api = params.api
         const thisRow = {};
-        //api.getAllColumns()
-        //.filter((c) => c.field !== "__check__" && !!c)
-        //.forEach(
-          //(c) => (thisRow[c.field] = params.getValue(params.id, c.field))
-        //);
+        api.getAllColumns()
+        .filter((c) => c.field !== "__check__" && !!c)
+        .forEach(
+          (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+        );
         
         return alert(apiDeleteHandler(params.id));
       };
@@ -50,7 +50,32 @@ const columns = [
       );
     },
   },
-  
+  {
+    field: "",
+    headerName: "Edit",
+    width: 150,
+    renderCell: (params) => {
+      const editHandler = (e) => {
+        e.stopPropagation()
+      
+        const api = params.api
+        const thisRow = {};
+        api.getAllColumns()
+        .filter((c) => c.field !== "__check__" && !!c)
+        .forEach(
+          (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+        );
+        const editID = (params.id)
+        return alert(editID);
+      };
+      console.log(editID)
+      return (
+        <Button variant="outlined" onClick={editHandler}>
+          <a href={`/edit/${editID}`}>Edit</a>
+        </Button>
+      );
+    },
+  }
 ];
 
 export default function TaskTable(props) {
